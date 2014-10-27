781b9ac57ab5aaa0ef3a79a625070a24885afbc2
Thomas Kjeldahl Nilsson <thomas@kjeldahlnilsson.net>
2014-10-27 22:23:44 +0100

    All crud ops present. Need cleanup, logging

--- habitboss_backend.go
+++ habitboss_backend.go
@@ -7,17 +7,18 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 type Habit struct {
 	Id            int
-	IntervalType  int
+	IntervalType  string
 	Description   string
 	LastPerformed string
 }
 
 func exampleHabit() Habit {
-	return Habit{Id: 12, IntervalType: 0, Description: "Walk the dog", LastPerformed: "2014-10-10T08:49:53+00:00"}
+	return Habit{Id: 12, IntervalType: "0", Description: "Walk the dog", LastPerformed: "2014-10-10T08:49:53+00:00"}
 }
 
 // ROOT
@@ -25,8 +26,10 @@ func root(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "ROOT")
 }
 
-// API, basic routing
-func currentHabits(w http.ResponseWriter, r *http.Request) {
+// API
+
+// Example: http://localhost:8080/api/getHabits
+func getHabits(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	habits, _ := load()
 	habitJson, _ := json.Marshal(habits)
@@ -34,32 +37,93 @@ func currentHabits(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+// Example: http://localhost:8080/api/createHabit?intervalType=%222%22&description=%22Take%20a%20swim%22&lastPerformed=%222014-10-10T08:49:53+00:00%22
 func createHabit(w http.ResponseWriter, r *http.Request) {
+	intervalType := r.URL.Query().Get("intervalType")
+	description := r.URL.Query().Get("description")
+	lastPerformed := r.URL.Query().Get("lastPerformed")
+
+	newHabit := Habit{Id: rand.Intn(100000000), IntervalType: intervalType, Description: description, LastPerformed: lastPerformed}
+
 	habits, _ := load()
-
-	// TODO extract params from request params
-	id := rand.Intn(100000)
-	intervalType := 0
-	description := "Work out"
-	lastPerformed := "2014-10-10T08:49:53+00:00"
-
-	newHabit := Habit{Id: id, IntervalType: intervalType, Description: description, LastPerformed: lastPerformed}
 	habits = append(habits, newHabit)
-
 	err := persist(habits)
+
 	if err == nil {
 		fmt.Println("Successfully added: ", newHabit)
 	} else {
 		fmt.Println("Error while adding habit: ", newHabit)
+		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
 
 func updateHabit(w http.ResponseWriter, r *http.Request) {
+	idToUpdate, _ := strconv.Atoi(r.URL.Query().Get("id"))
+	updatedDescription := r.URL.Query().Get("description")
+	updatedLastPerformed := r.URL.Query().Get("lastPerformed")
 
+	habits, _ := load()
+
+	// Where in the habit list is it?
+	indexOfDeletion := -1
+	for index, element := range habits {
+		if element.Id == idToUpdate {
+			indexOfDeletion = index
+		}
+	}
+
+	if indexOfDeletion == -1 {
+		fmt.Println("No habit with id: ", idToUpdate)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Update it
+	oldHabit := habits[indexOfDeletion]
+	newHabit := Habit{Id: oldHabit.Id, IntervalType: oldHabit.IntervalType, Description: updatedDescription, LastPerformed: updatedLastPerformed}
+	oldHabit.Description = updatedDescription
+	oldHabit.LastPerformed = updatedLastPerformed
+	habits[indexOfDeletion] = newHabit
+
+	err := persist(habits)
+	if err == nil {
+		fmt.Println("Successfully updated habit: ", idToUpdate)
+	} else {
+		fmt.Println("Error while updating habit: ", idToUpdate)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
 
+// Example:    http://localhost:8080/api/deleteHabit?id=98081
 func deleteHabit(w http.ResponseWriter, r *http.Request) {
+	idToDelete, _ := strconv.Atoi(r.URL.Query().Get("id"))
 
+	habits, _ := load()
+
+	// Where in the habit list is it?
+	indexOfDeletion := -1
+	for index, element := range habits {
+		if element.Id == idToDelete {
+			indexOfDeletion = index
+		}
+	}
+
+	if indexOfDeletion == -1 {
+		fmt.Println("No habit with id: ", idToDelete)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Remove it. Not exactly pretty, but most ideomatic I could find
+	habits = append(habits[:indexOfDeletion], habits[indexOfDeletion+1:]...)
+
+	err := persist(habits)
+	if err == nil {
+		fmt.Println("Successfully removed habit: ", idToDelete)
+	} else {
+		fmt.Println("Error while removing habit: ", idToDelete)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
 
 /* TODO Routing to each endpoint
@@ -85,7 +149,7 @@ func main() {
 	http.HandleFunc("/", root)
 	http.HandleFunc("/webconsole", webconsole)
 
-	http.HandleFunc("/api/currentHabits", currentHabits)
+	http.HandleFunc("/api/getHabits", getHabits)
 	http.HandleFunc("/api/createHabit", createHabit)
 	http.HandleFunc("/api/updateHabit", updateHabit)
 	http.HandleFunc("/api/deleteHabit", deleteHabit)
